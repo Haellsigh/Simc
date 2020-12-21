1fa60caf6ba92cd9db8336f21735e7e45142627e
Haellsigh <haellsigh@gmail.com>
2020-12-21 21:24:44 +0100

    Begin observer base

--- src/observer/base.hh
+++ src/observer/base.hh
@@ -1,3 +1,28 @@
 #pragma once
 
-namespace simc::observer {}
+#include "variable.hh"
+
+namespace simc::observer {
+
+template <std::size_t input_size,
+          std::size_t sensor_output_size,
+          std::size_t observer_state_size,
+          std::size_t system_state_size>
+struct Base : public VariableBase {
+  using input_t          = std::array<double, input_size>;
+  using sensor_output_t  = std::array<double, sensor_output_size>;
+  using observer_state_t = std::array<double, observer_state_size>;
+  using system_state_t   = std::array<double, system_state_size>;
+
+  virtual ~Base() = default;
+
+  virtual observer_state_t F(sensor_output_t const&  z_k,
+                             observer_state_t const& xi_k,
+                             input_t const&          u_k) = 0;
+
+  virtual system_state_t H(sensor_output_t const&  z_k,
+                           observer_state_t const& xi_k,
+                           input_t const&          u_k) = 0;
+};
+
+}  // namespace simc::observer
